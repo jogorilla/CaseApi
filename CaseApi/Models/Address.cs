d860e98bc6bf041cd49ae8fd6c02ccdcc3742fa2
jogorilla <jochemodie@gmail.com>
2022-11-17 08:52:00 +0100

    Made all Address fields required

--- CaseApi/Models/Address.cs
+++ CaseApi/Models/Address.cs
@@ -11,14 +11,19 @@ namespace CaseApi.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Street { get; set; }
 
+        [Required]
         public int HouseNumber { get; set; }
 
+        [Required]
         public string PostalCode { get; set; }
 
+        [Required]
         public string City { get; set; }
 
+        [Required]
         public string Country { get; set; }
     }
 }
