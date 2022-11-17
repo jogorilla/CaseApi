196b92634d3046600fc62083875d354c5f137224
jogorilla <jochemodie@gmail.com>
2022-11-17 08:43:03 +0100

    Add packages and API endpoints for part 1

---
+++ CaseApi/Models/AddressContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseApi.Models
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
+
